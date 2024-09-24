5764fec4d805c06d1f13f3623de7146ec61ff5e5
Dmitry Chubarov <dime@twowls.org>
2024-09-24 20:37:20 +0400

    Improve font import (Inter).

--- fe/src/index.tsx
+++ fe/src/index.tsx
@@ -4,7 +4,11 @@ import {RouterProvider} from "react-router-dom";
 import defaultRouter from "./Routing";
 import reportWebVitals from "./reportWebVitals";
 
-import "@fontsource/inter";
+// Inter font
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/400-italic.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/600-italic.css";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
