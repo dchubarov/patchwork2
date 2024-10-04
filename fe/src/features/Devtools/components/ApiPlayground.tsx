6c32f60e02f4bd625099b2a640e50fb17af488cf
Dmitry Chubarov <dime@twowls.org>
2024-10-04 15:59:49 +0400

    Create ApiPlayground in DevTools

---
+++ fe/src/features/Devtools/components/ApiPlayground.tsx
@@ -0,0 +1,69 @@
+import React, {useState} from "react";
+import {IndexedLayoutChildProps} from "../../../lib/pageLayoutTypes";
+import {Box, Chip, FormControl, IconButton, Input, Typography} from "@mui/joy";
+import axios from "axios";
+import "axios-retry";
+import {CheckCircleOutline as SuccessIcon, PlayArrow as RunIcon, Warning as WarningIcon} from "@mui/icons-material";
+
+type RequestState =
+    | { status: "empty" }
+    | { status: "loading" }
+    | { status: "success", response: any }
+    | { status: "failure", response: any };
+
+const ApiPlayground: React.FC<IndexedLayoutChildProps> = () => {
+    const [requestState, setRequestState] = useState<RequestState>({status: "empty"})
+    const [apiUrl, setApiUrl] = useState("");
+
+    const apiPrefix = process.env.REACT_APP_API_ROOT + "/";
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setRequestState({status: "loading"});
+
+        axios.get(apiPrefix + apiUrl, {"axios-retry": {retries: 3}})
+            .then((response) => setRequestState({status: "success", response: response}))
+            .catch((reason) => setRequestState({status: "failure", response: reason}))
+    }
+
+    return (<>
+        <form onSubmit={handleSubmit}>
+            <FormControl>
+                <Input
+                    value={apiUrl}
+                    required
+                    autoFocus
+                    startDecorator={<><Chip variant="solid" color="success" sx={{mr: 1}}>GET</Chip> {apiPrefix}</>}
+                    disabled={requestState.status === "loading"}
+                    onChange={(e) => setApiUrl(e.target.value)}
+                    endDecorator={
+                        <IconButton
+                            type="submit"
+                            loading={requestState.status === "loading"}
+                            variant="solid"
+                            color="primary">
+                            <RunIcon/>
+                        </IconButton>
+                    }/>
+            </FormControl>
+        </form>
+
+        {(requestState.status === "success" || requestState.status === "failure") &&
+            <Box mt={2}>
+                <Typography
+                    level="body-sm"
+                    fontWeight="bold"
+                    color={requestState.status === "success" ? "success" : "danger"}
+                    startDecorator={requestState.status === "success" ? <SuccessIcon/> : <WarningIcon/>}>
+                    {(requestState.status === "success" ? "Success: " : "Error: ") + (requestState.response.status || "unknown")}
+                </Typography>
+
+                <Typography level="body-sm" fontWeight="bold" ml={3} mt={1}>Response:</Typography>
+                <Typography component="pre" level="body-sm" ml={3} /*fontFamily="monospace"*/>
+                    {JSON.stringify(requestState.response.data || requestState.response, null, 2)}
+                </Typography>
+            </Box>}
+    </>);
+}
+
+export default ApiPlayground;
--- fe/src/features/Devtools/pages/DevtoolsPage.tsx
+++ fe/src/features/Devtools/pages/DevtoolsPage.tsx
@@ -2,6 +2,7 @@ import React, {useEffect} from "react";
 import PageLayout from "../../../components/PageLayout";
 import ViewContextPlayground from "../components/ViewContextPlayground";
 import {useActiveView} from "../../../lib/useActiveView";
+import ApiPlayground from "../components/ApiPlayground";
 
 const DevtoolsPage: React.FC = () => {
     const {ejectView} = useActiveView();
@@ -12,6 +13,10 @@ const DevtoolsPage: React.FC = () => {
 
     return (
         <PageLayout.Indexed>
+            <ApiPlayground
+                tabKey="api-playground"
+                tabCaption="API playground"/>
+
             <ViewContextPlayground
                 tabKey="view-context-playground"
                 tabCaption="View context playground"/>
