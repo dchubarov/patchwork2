57ce6c81a9621e25c069e1d7f837a022e49ff570
Dmitry Chubarov <dime@twowls.org>
2024-10-11 17:05:56 +0400

    Improve color label components.

--- fe/src/lib/theme.ts
+++ fe/src/lib/theme.ts
@@ -46,16 +46,16 @@ const defaultColorLabels: ColorLabelPalette = {
         800: "#9d174d",
         900: "#831843",
     },
-    emerald: {
-        100: "#d1fae5",
-        200: "#a7f3d0",
-        300: "#6ee7b7",
-        400: "#34d399",
-        500: "#10b981",
-        600: "#059669",
-        700: "#047857",
-        800: "#065f46",
-        900: "#064e3b",
+    lime: {
+        100: '#ecfccb',
+        200: '#d9f99d',
+        300: '#bef264',
+        400: '#a3e635',
+        500: '#84cc16',
+        600: '#65a30d',
+        700: '#4d7c0f',
+        800: '#3f6212',
+        900: '#365314'
     },
     indigo: {
         100: "#e0e7ff",
@@ -109,7 +109,7 @@ const appTheme = extendTheme({
 
 export default appTheme;
 
-export function labelColorsByName(name?: string, theme?: Theme): ColorLabelPaletteRange {
+export function labelColorsByName(name?: string | null, theme?: Theme): ColorLabelPaletteRange {
     const t = theme || appTheme;
     const p = name ? t.palette.colorLabel[name] : undefined;
     return p || t.palette.neutral;
