9588418dd1073575ea771cdc74088a17a78830a9
Dmitry Chubarov <dime@twowls.org>
2024-10-12 17:14:04 +0400

    Change default font to Noto Sans.

--- fe/src/index.tsx
+++ fe/src/index.tsx
@@ -7,18 +7,6 @@ import App from "./App";
 import ErrorPage from "./ErrorPage";
 import SampleView from "./SampleView";
 
-// Inter font
-import "@fontsource/inter/300.css";
-import "@fontsource/inter/300-italic.css";
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/400-italic.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/500-italic.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/600-italic.css";
-import "@fontsource/inter/700.css";
-import "@fontsource/inter/700-italic.css";
-
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
