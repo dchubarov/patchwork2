7a4111d3968c9e75cdd97cd8a9456c0203411199
Dmitry Chubarov <dime@twowls.org>
2024-10-26 17:14:52 +0400

    Add support for .svg files import (@svgr/webpack).

--- fe/config-overrides.js
+++ fe/config-overrides.js
@@ -1,8 +1,22 @@
 const {TsconfigPathsPlugin} = require("tsconfig-paths-webpack-plugin");
 
-module.exports = {
-    "webpack": function(config) {
-        config.resolve.plugins.push(new TsconfigPathsPlugin());
-        return config;
-    }
+module.exports = function override(config/*, env*/) {
+    config.resolve.plugins.push(new TsconfigPathsPlugin());
+
+    config.module.rules[1].oneOf.splice(0, 0, {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+    });
+
+    /*
+    console.log(`
+WEBPACK CONFIG
+==============
+${JSON.stringify(config, null, 2)}
+==================
+END WEBPACK CONFIG`
+    );
+    */
+
+    return config;
 }
