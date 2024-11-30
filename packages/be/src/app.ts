98dee5806a00cc778f065dae10bf8997c393fe58
Dmitry Chubarov <dime@twowls.org>
2024-11-30 22:05:31 +0400

    Add logging framework (pino)

--- packages/be/src/app.ts
+++ packages/be/src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import pino from 'pino-http';
+import { rootLogger } from './logging';
 
 const app = express();
+
+/* More options: https://www.npmjs.com/package/pino-http */
+app.use(pino({ logger: rootLogger, useLevel: 'trace' }));
 
 app.get('/server-info', async (_req, res) => {
   res.send('OK');
 });
 
 app.listen(5555, () => {
-  console.log(`Server is running`);
+  rootLogger.info(`Server is running`);
 });
